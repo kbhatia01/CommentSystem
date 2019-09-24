d02a6ba4c1e22db78d57cb4b8e604e0dba2890ba
Karan bhatia <Karan.bhatia.16cse@bml.edu.in>
2019-09-24 14:53:20 +0530

    controller for comments

--- RentoMojo/controller/CommentController.js
+++ RentoMojo/controller/CommentController.js
@@ -1,5 +1,51 @@
-module.exports= function commentController(cmnt){
-    
-    
-    return{};
+module.exports = function commentController(cmnt) {
+
+    function patch(req, res) {
+        cmnt.findById(req.params.Cid, (err, comment) => {
+            if (err) {
+                res.status(500).send(err)
+            }
+            else {
+                req.cmnt = comment;
+                const coment = req.cmnt;
+                // console.log(req);
+
+                if (req.body._id) {
+                    delete req.body._id;
+                }
+                Object.entries(req.body).forEach((element) => {
+                    const key = element[0];
+                    const value = element[1];
+                    cmnt[key] = value;
+
+                })
+                coment.save((err) => {
+                    if (err) {
+                        return res.json(err);
+                    }
+                    else {
+                        return res.json(coment);
+                    }
+                });
+            }
+
+        })
+    }
+
+    function post(req, res) {
+        const ress = new cmnt(req.body);
+
+        ress.save();
+        res.status(201)
+        return res.json(ress);
+    }
+    function get(req, res) {
+        cmnt.find((err, cmnts) => {
+            if (err) {
+                return res.send(err);
+            }
+            return res.json(cmnts);
+        })
+    }
+    return { patch, post, get };
 }
--- RentoMojo/routes/commentRoute.js
+++ RentoMojo/routes/commentRoute.js
@@ -6,6 +6,9 @@ function routes(cmnt) {
 
 const controller=commentController(cmnt);
 const router = express.Router(cmnt);
+router.route("/comment").post(controller.post);
+router.route("/comment").get(controller.get);
+router.route("/comment/:Cid").get(controller.patch);
 return router;
 
 }
