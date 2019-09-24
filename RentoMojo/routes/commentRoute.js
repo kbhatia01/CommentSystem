d1f8370af4d5035432b8ee0315f499f34aa36ef1
Karan bhatia <Karan.bhatia.16cse@bml.edu.in>
2019-09-24 15:03:12 +0530

    Final server

--- RentoMojo/routes/commentRoute.js
+++ RentoMojo/routes/commentRoute.js
@@ -8,7 +8,7 @@ const controller=commentController(cmnt);
 const router = express.Router(cmnt);
 router.route("/comment").post(controller.post);
 router.route("/comment").get(controller.get);
-router.route("/comment/:Cid").get(controller.patch);
+router.route("/comment/:Cid").patch(controller.patch);
 return router;
 
 }
